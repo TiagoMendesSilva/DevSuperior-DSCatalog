216b540fa6d79a5c730dc331fcf4dda01b6677e9
tiago <tiagoms@ciandt.com>
2021-06-13 00:05:01 -0300

    Adiciona pacote entities e  classe Category e pom.xml adiciona org.apache.commons.lang

---
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
@@ -0,0 +1,44 @@
+package com.devsuperior.dscatalog.entities;
+
+import java.io.Serializable;
+
+public class Category implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private Long id;
+    private String name;
+
+    public Category(){
+    }
+    public Category(Long id, String name){
+        this.id = id;
+        this.name = name;
+    }
+    public Long getId(){
+        return id;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setId(Long id){
+        this.id = id;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Category category = (Category) o;
+
+        return new org.apache.commons.lang.builder.EqualsBuilder().append(id, category.id).isEquals();
+    }
+
+    @Override
+    public int hashCode() {
+        return new org.apache.commons.lang.builder.HashCodeBuilder(17, 37).append(id).toHashCode();
+    }
+}
